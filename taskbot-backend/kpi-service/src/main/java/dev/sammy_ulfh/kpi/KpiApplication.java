cfa92ead17e27fda8e418c33ba7f20bfcf9f57f6
sammy-ulfh <185651000+sammy-ulfh@users.noreply.github.com>
2026-04-24 17:34:26 -0600

    kafka fix

--- taskbot-backend/kpi-service/src/main/java/dev/sammy_ulfh/kpi/KpiApplication.java
+++ taskbot-backend/kpi-service/src/main/java/dev/sammy_ulfh/kpi/KpiApplication.java
@@ -2,7 +2,9 @@ package dev.sammy_ulfh.kpi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
+@EnableKafka
 @SpringBootApplication
 public class KpiApplication {
 
