a08660e60a8a690a8896ae8ad9e1cf89112b5407
sammy-ulfh <185651000+sammy-ulfh@users.noreply.github.com>
2026-04-29 22:53:36 -0600

    AI-SERVICE (FROM DIANA) && started task-service

--- taskbot-backend/ai-service/src/test/java/dev/sammy_ulfh/ai/AiApplicationTests.java
+++ taskbot-backend/ai-service/src/test/java/dev/sammy_ulfh/ai/AiApplicationTests.java
@@ -1,10 +1,7 @@
-//ai-service/src/test/java/dev/sammy_ulfh/ai/AiApplicationTests.java
 package dev.sammy_ulfh.ai;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class AiApplicationTests {
 
 	@Test
