ba825f56d19f838260bd9153c1f3fc75d8bebbc3
delsa21 <A01644911@tec.mx>
2026-05-21 20:44:25 -0600

    Limpieza de código

--- taskbot-backend/kpi-service/src/main/java/dev/sammy_ulfh/kpi/KpiApplication.java
+++ taskbot-backend/kpi-service/src/main/java/dev/sammy_ulfh/kpi/KpiApplication.java
@@ -2,9 +2,7 @@ package dev.sammy_ulfh.kpi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.kafka.annotation.EnableKafka;
 
-@EnableKafka
 @SpringBootApplication
 public class KpiApplication {
 
